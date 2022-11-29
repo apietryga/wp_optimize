28c98fb4656f6d64e51a2cc70cd81fd1b5099cb9
apietryga <antek.pietryga@gmail.com>
2022-11-29 08:54:55 +0100

    fix

--- index.js
+++ index.js
@@ -1,47 +1,98 @@
-/*
-
-
-skip-checks:true
-*/
-
-
-console.log('hi5')
-const module = {
+console.log('hi6')
+const ap_module = {
+  tryingLimit: 10,
   removeFooter() {
     const footers = [
       {
-        class: ".site-footer .twp-footer .footer-active .show",
+        class: ".site-footer.twp-footer.footer-active.show",
         text: "Copyright All right reserved Theme: Default Mag by ThemeInWP"    
       }
 
     ] 
-    footers.forEach(foot => {
+    for( const foot of footers ){
       const footer = document.querySelector(foot.class)
-      console.log({ foot, footer })
       if(footer != null && footer.innerText == foot.text){
-        footer.innerHTML = ""
+        footer.innerHTML = "Made with ❤️ by " + location.host + " team." 
         return true
       }
       return false
-    })
+    }
+  },
+  acceptCookies(){
+    document.cookie = "ap_module_popup=true";
+    document.querySelector('.customCookiesPopup').style.display = "none"
+  },
+  makeCookiesPopup(){
+    if(this.functions.getCookie('ap_module_popup')){ return }
+    const cookies = document.createElement("div")
+    cookies.className = "customCookiesPopup"
+    cookies.innerHTML = /*html*/`
+      <style>
+        .customCookiesPopup {
+          position:fixed;
+          display:flex;
+          flex-direction:column;
+          right:5px;
+          bottom:5px;
+          border-radius:5px;
+          background:linear-gradient(#222, #444);
+          font-weight:bold;
+          color:#fff;
+        }
+        .customCookiesPopup p {
+          text-align:center;
+          margin:0;
+          padding:2rem;
+          padding-bottom:.25rem;
+          font-size:1.2rem;
+        }
+        .customCookiesPopup > div {
+          display:flex;
+          justify-content:end;
+          padding:.5rem 1rem;
+        }
+        .customCookiesPopup > div > a{
+          border:3px solid #fff;
+          text-decoration:none;
+          padding:.5rem;
+          margin:.25rem;
+          border-radius:3px;
+          color:inherit;
+        }
+      </style>
+      <p>W celu zapewnienia wyższej jakości usług <br/>na tej stronie użyto plików cookie.</p>
+      <div>
+        <a href="https://ec.europa.eu/info/cookies_pl" target="_blank">Więcej</a>
+        <a href="#" onclick="ap_module.acceptCookies()"> Akceptuję </a>
+      </div>
+    `
+    document.body.append(cookies)
+  },
+  functions : {
+    getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(';').shift();
+    }
   },
   async inPromise( functionName ) {
     return await new Promise( res => {
       const interval = setInterval(()=>{
         const resp = this[functionName]()
-        if(resp){
+        console.log({resp})
+        if(resp || this.tryingLimit < 1){
           clearInterval(interval)
-          res(resp)
+          res( resp )
         }
+        this.tryingLimit--
       },300)
     })
   },
   runWhenPageIsReady() {
     window.addEventListener('load', async () => {
-      console.log('windowloaded', { document })
-      const inProm = await this.inPromise('removeFooter')
-      console.log({ inProm })
+      await this.inPromise('removeFooter')
+      this.makeCookiesPopup()
     })
   }
 }
-module.runWhenPageIsReady()
+ap_module.runWhenPageIsReady()
