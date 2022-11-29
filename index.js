7f5467e205aba50149c5ab3582154e1a953608d4
apietryga <antek.pietryga@gmail.com>
2022-11-29 06:47:59 +0100

    f

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-console.log('hellooo')
-document.addEventListener('load', () => {
 
-  console.log('body:', document.body);
+const footers = [
+  {
+    class: ".site-footer .twp-footer .footer-active .show",
+    text: "Copyright All right reserved Theme: Default Mag by ThemeInWP"    
+  }
 
+]
 
-
-
-
+footers.forEach(foot => {
+  const footer = document.querySelector(foot.class)
+  if(footer != null && footer.innerText == foot.text){
+    footer.innerHTML = ""
+  }
 })
