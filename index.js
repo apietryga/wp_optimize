40442a100fe668ac1d334f2848e600d2890a57ff
apietryga <antek.pietryga@gmail.com>
2022-11-29 07:21:00 +0100

    aa

--- index.js
+++ index.js
@@ -4,19 +4,32 @@
 skip-checks:true
 */
 
+
 console.log('hi4')
-const footers = [
-  {
-    class: ".site-footer .twp-footer .footer-active .show",
-    text: "Copyright All right reserved Theme: Default Mag by ThemeInWP"    
+const module = {
+  removeFooter() {
+    const footers = [
+      {
+        class: ".site-footer .twp-footer .footer-active .show",
+        text: "Copyright All right reserved Theme: Default Mag by ThemeInWP"    
+      }
+
+    ] 
+    footers.forEach(foot => {
+      const footer = document.querySelector(foot.class)
+      if(footer != null && footer.innerText == foot.text){
+        footer.innerHTML = ""
+      }
+      console.log({ foot, footer })
+    })
+  },
+  runWhenPageIsReady() {
+    document.addEventListener('load', () => {
+      console.log('documentloaded', { document })
+    })
+    console.log({ document })
+
   }
 
-]
-
-footers.forEach(foot => {
-  const footer = document.querySelector(foot.class)
-  if(footer != null && footer.innerText == foot.text){
-    footer.innerHTML = ""
-  }
-  console.log({ foot, footer })
-})
+}
+module.runWhenPageIsReady()
